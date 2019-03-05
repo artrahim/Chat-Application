1a408ce7a97bd374d8e1ec08f29b309c1379a3d9
Artemiss Rahim <artemiss.rahim@ucalgary.ca>
2019-03-05 11:50:41 -0700

    implemented users

--- index.js
+++ index.js
@@ -4,15 +4,64 @@ const express = require('express'),
     io = require('socket.io')(http),
     path = require('path');
 
+let firstNames = ["Aloof", "Auspicious", "Boorish", "Cagey", "Caustic", "Dapper",
+"Erratic", "Ethereal", "Guileless", "Heady", "Insidious", "Obtuse", "Petulant",
+"Placid", "Plucky", "Sordid", "Tenacious", "Vivacious", "Wistful"];
+
+let lastNames = ["Axolotl", "Salamander", "Frog", "Turtle", "Buffalo", "Lizard",
+"Condor", "Cormorant", "Capybara", "Sheep", "Quokka", "Platypus", "Sugar Glider",
+"Wombat", "Koala", "Sea Lion", "Kangaroo", "Kakapo", "Praying Mantis", "Camel",
+"Nutria", "Antelope", "Tapir", "Echidna", "Hedgehog", "Pangolin", "Opossum"];
+
+let currentUsers = [];
+
+let chatHistory = [];
+let username = "";
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//TODO: bold user messages, allow changing usernames, allow changing colour of username, implement cookies, add scrollbar, align text to bottom
 io.on('connection', function(socket){
+
+    // give connected user past messages
+    socket.emit('get messages', chatHistory);
+
+    // generate random username
+    username = generateName();
+
+    // set initial username
+    socket.emit('set username', username);
+    socket.username = username;
+
+    // add username to list of users
+    currentUsers.push(username);
+
+    // show updated list of users
+    io.emit('add user', currentUsers);
+
     socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+        //check if message is empty
+        if(msg.trim() !== ""){
+            //calculate time message is sent
+            let messageTime = calculateTime();
+
+            let message = messageTime + "  " + socket.username + ": " + msg;
+
+            chatHistory.push(message);
+
+            io.emit('chat message', message);
+        }
+    });
+
+
+    socket.on('disconnect', function () {
+        currentUsers.splice(currentUsers.indexOf(socket.username));
+
+        io.emit('remove user', socket.username);
     });
 });
 
@@ -20,3 +69,37 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+function calculateTime(){
+    let time;
+    const date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+
+    if(minute < 10){
+        minute = "0" + minute;
+    }
+
+    if(hour > 12) {
+        hour = hour - 12;
+        time = hour + ":" + minute + "pm";
+    } else {
+        time = hour + ":" + minute + "am";
+    }
+
+    return time;
+}
+
+function generateName(){
+    let name;
+
+    // repeat generation until it creates a name not currently used
+    do {
+        let randFirst = firstNames[Math.floor(Math.random() * firstNames.length)];
+        let randLast = lastNames[Math.floor(Math.random() * lastNames.length)];
+
+        name = randFirst+" "+randLast;
+    } while(currentUsers.includes(name));
+
+    return name;
+}
--- public/client.js
+++ public/client.js
@@ -1,12 +1,40 @@
 $(function () {
     const socket = io();
+
     $('form').submit(function(e){
         e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
+        socket.emit('chat message', $('#messageInput').val());
+        $('#messageInput').val('');
         return false;
     });
+
     socket.on('chat message', function(msg){
         $('#messages').append($('<li>').text(msg));
     });
+
+    socket.on('change name', function(name){
+
+    });
+
+    socket.on('set username', function(name){
+        $('#identifier').append(" "+name);
+    });
+
+    socket.on('add user', function (users) {
+        $('#user-list').empty();
+        $.each(users, function (i) {
+            $('#user-list').append($('<li>'+users[i]+'</li>'));
+        });
+    });
+
+    socket.on('remove user', function (user) {
+        $('li:contains('+user+')').remove();
+    });
+
+    socket.on('get messages', function (chat) {
+        $.each(chat, function (i) {
+            $('#messages').append($('<li>'+chat[i]+'</li>'));
+        });
+    })
+
 });
